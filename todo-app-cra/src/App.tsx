7fbdbd3a5840386231608f0c82603b053bdb6ae1
casdidier <cheung.didier@gmail.com>
2020-11-30 19:05:25 +0100

    setup AddToDoForm

--- todo-app-cra/src/App.tsx
+++ todo-app-cra/src/App.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import { TodoListItem } from './TodoListItem';
+import React, { useState } from 'react';
+import { TodoList } from './TodoList';
+import { AddTodoForm } from './AddTodoForm';
 
-const todos: Todo[] = [
+const initialTodos: Todo[] = [
   {
     text: 'Walk the dog',
     complete: false,
@@ -13,11 +14,31 @@ const todos: Todo[] = [
 ];
 
 function App() {
+  const [todos, setTodos] = useState(initialTodos);
+
+  const toggleTodo: ToggleTodo = (selectedTodo: Todo) => {
+    const newTodos = todos.map(todo => {
+      if (todo === selectedTodo) {
+        return {
+          ...todo,
+          complete: !todo.complete,
+        };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  const addTodo: AddTodo = (text: string) => {
+    const newTodo = { text, complete: false };
+    setTodos([...todos, newTodo]);
+  };
+
   return (
-    <ul>
-      <TodoListItem todo={todos[0]} />
-      <TodoListItem todo={todos[1]} />
-    </ul>
+    <>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <AddTodoForm addTodo={addTodo} />
+    </>
   );
 }
 
