b994b524bafa2def27cce9334966d91db2aa621f
casdidier <cheung.didier@gmail.com>
2020-11-30 18:37:13 +0100

    todolistitem implementation

--- todo-app-cra/src/TodoListItem.tsx
+++ todo-app-cra/src/TodoListItem.tsx
@@ -1,5 +1,24 @@
 import React from 'react';
 
-export const TodoListItem = () => {
-  return <li>content</li>;
+/* Obsolete since it is in types.d.ts
+interface Todo {
+  text: string;
+  complete: boolean;
+}
+*/
+
+interface Props {
+  todo: Todo;
+}
+
+export const TodoListItem: React.FC<Props> = ({ todo }) => {
+  return (
+    <li>
+      <label
+        style={{ textDecoration: todo.complete ? 'line-through' : undefined }}
+      >
+        <input type="checkbox" checked={todo.complete} /> {todo.text}
+      </label>
+    </li>
+  );
 };
