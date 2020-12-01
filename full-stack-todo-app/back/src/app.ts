a21b8572904c439f960d1d3bb5784fbb6a90d6cc
casdidier <cheung.didier@gmail.com>
2020-12-01 17:39:09 +0100

    fixed routing issue + body undefined due to missing body parser

--- full-stack-todo-app/back/src/app.ts
+++ full-stack-todo-app/back/src/app.ts
@@ -1,14 +1,15 @@
 import express, { Express } from "express"
 import mongoose from "mongoose"
 import cors from "cors"
-import todoRoutes from "./routes"
+import router from "./routes"
 
 const app: Express = express()
 
 const PORT: string | number = process.env.PORT || 4000
 
 app.use(cors())
-app.use(todoRoutes)
+app.use(express.json()) // body-parser built in Express
+app.use(router)
 
 const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.l1yfj.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
 const options = { useNewUrlParser: true, useUnifiedTopology: true }
